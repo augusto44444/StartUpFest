6e8baffa7fb36805ab7b13e095950b33ef018086
Augusto <augusto.h.amaral@gmail.com>
2020-04-13 02:52:37 -0300

    [update] retirando a pesquisa da tela de avaliações

--- src/components/Layout/mainContent/Rates/index.jsx
+++ src/components/Layout/mainContent/Rates/index.jsx
@@ -104,7 +104,7 @@ class MainContent extends Component {
                                                         filledIcon={<i className="fa fa-star" style={{ color: '#5a17f8' }} />}
                                                         color='#5a17f8'
                                                     />
-                                                    <p style={{fontSize: '0.6em'}}>Média de {item.qtdRates} avaliações</p>
+                                                    <p style={{ fontSize: '0.6em' }}>Média de {item.qtdRates} avaliações</p>
                                                 </div>
                                                 <Badge variant='warning'>{parseInt(item.rate) / 2}</Badge>
                                                 <Accordion.Toggle as={Button} variant="link" eventKey="0">
@@ -170,50 +170,13 @@ class MainContent extends Component {
 
 
 
-    filter() {
-        if (this.state.filter) {
-            const array = this.state.startups.find(item => {
-                if (item.name.toLowerCase().indexOf(this.state.filter.toLocaleLowerCase()) != -1) {
-                    return item
-                }
-            })
-
-            // console.log([array])
-            if (array && [array] && [array].length > 0) {
-                // console.log("foi")
-                this.setState({ startups: [array], filter: '' })
-            } else {
-                this.setState({ filter: '' })
-            }
-        } else {
-            this.setState({ startups: this.props.startups.allStartups, filter: '' })
-        }
-    }
 
 
     render() {
 
         return (
             <div className='MainContent'>
-
-                <Row>
-                    <Col md='10' sm='10'>
-                        <Form.Control type='text' onKeyPress={e => {
-                            if (e.key == 'Enter') {
-                                this.filter()
-                            }
-                        }} value={this.state.filter} onChange={e => this.setState({ filter: e.target.value })} placeholder='Pesquisar por Startup' />
-                    </Col>
-                    <Col md='2' sm='2'>
-                        <Button type='submit' className='button' variant='info' onClick={() => {
-                            this.filter()
-                        }}>{this.props.startups && this.props.startups.allStartups ? this.state.startups.length == this.props.startups.allStartups.length ? 'Pesquisar' : 'Voltar' : 'Pesquisar'}</Button>
-                    </Col>
-                </Row>
-                <hr />
-                <div>
-                    {this.state.Rates.length > 0 ? this.renderStartup() : <Spinner animation="border" className='loading' />}
-                </div>
+                {this.state.Rates.length > 0 ? this.renderStartup() : <Spinner animation="border" className='loading' />}
             </div>
         );
     }
